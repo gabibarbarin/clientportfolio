36ba0e23373d185a95acc68afc30cba3e535544f
Gabriel Barbarin Gorostegui <gabi.barbarin@hotmail.com>
2021-10-25 16:50:11 -0300

    Agregue las vistas responsives del Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Home = () => {
   return (
     <div className="container img-home home-container">
-      <div className="col-8 d-flex flex-column heading-away welcome-text">
+      <div className="col-11 col-lg-8 d-flex flex-column heading-away welcome-text">
         <h3>Hola, soy Gabriel Barbarin</h3>
         <h1>Desarrollador web Full Stack Jr basado en ReactJS y NodeJS.</h1>
       </div>
