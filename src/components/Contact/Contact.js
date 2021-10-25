47d28e5e7dd97f833804feab1ec38415ecbe2f34
Gabriel Barbarin Gorostegui <gabi.barbarin@hotmail.com>
2021-10-25 11:12:12 -0300

    Cree el componente Home (No responsive) y un componente Contact para probar que el Header no sufria de rerenderizados

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div className="container heading-away">
+      Contacto
+      <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+    </div>
+  );
+};
+export default Contact;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+const Home = () => {
+  return (
+    <div className="container img-home home-container">
+      <div className="col-8 d-flex flex-column heading-away welcome-text">
+        <h3>Hola, soy Gabriel Barbarin</h3>
+        <h1>Desarrollador web Full Stack Jr basado en ReactJS y NodeJS.</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
